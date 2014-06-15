42768729fa37eaca7c8aaa3111a7ec4169a6dbc5
Raymond Kiekens <raymond@oberon.nl>
2014-06-15 21:22:19 +0200

    background

---
+++ app/views/event/index.blade.php
@@ -0,0 +1,17 @@
+@extends("layout")
+@section("content")
+<div class="left-area2">
+	<section class="left-inner2 box clearfix ">
+		<img class="img-rounded " src="images/events/Jantje-verjaardag.jpg"/>
+	</section>
+
+</div>
+<div class="left-area1">
+	<section class="left-inner1 box">
+		@include("blocks/contact")
+	</section>
+</div>
+@stop
+@section("footer")
+@parent
+@stop
